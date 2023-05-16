849206a395cf182a7aab540446cfd7be3279ec28
Akash Bojja <122789336+akashs-bit@users.noreply.github.com>
2023-05-16 11:14:26 +0530

    Add files via upload

---
+++ Javascript/Speak/main.js
@@ -0,0 +1,8 @@
+const speakBtn=document.querySelector("#play")
+const text=document.querySelector("#text")
+
+
+speakBtn.addEventListener("click",()=>{
+    const speech=new SpeechSynthesisUtterance(text.value)
+    speechSynthesis.speak(speech)
+})
